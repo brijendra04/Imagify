75bd672d0474b2b37e161a0c9165d03929db48a1
Brijendra Pratap <brijendraoffice@gmail.com>
2025-03-16 19:34:39 +0530

    update

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-
+import { stepsData } from "../assets/assets"
 const Steps = () => {
+
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center text-center my-32">
       <h1 className="text-3xl sm:text-4xl font-semibold mb-2">How it works</h1>
       <p className="text-text-lg text-gray-600 mb-8">
         Transform Words Into Stunning Images
       </p>
 
-      <div>
-        {stepsData.map((step, index) => (
-          <div key={index}>
-            <div className="flex items-center gap-2">
-              <img src={step.icon} alt="" />
-              <h3 className="text-lg font-semibold">{step.title}</h3>
+      <div className="space-y-4 w-full max-w-3xl text-sm">
+        {stepsData.map((item, index) => (
+          <div key={index} className="flex items-center gap-4 p-5 px-8 bg-white/20 shadow-md border cursor-pointer hover:scale-[1.02] transition-all duration-300 rounded-lg">
+            <img width={40} src={item.icon} alt="" />
+            <div>      
+              <h2 className="text-xl font-medium">{item.title}</h2>
             </div>
-            <p className="text-gray-600">{step.description}</p>
+            <p className="text-gray-500">{item.description}</p>
           </div>
         ))}
       </div>
